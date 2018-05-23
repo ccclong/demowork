191c75e479298f8c066b59f611f8f073bef37463
cccchen <961320174@qq.com>
2018-05-23 16:51:51 +0800

    Initial commit

---
+++ webtest/src/main/java/com/test/Test.java
@@ -0,0 +1,27 @@
+package com.test;
+
+public class Test {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Test(String name) {
+        this.name = name;
+    }
+    public Test() {
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
